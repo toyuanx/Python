5987caf9bfa8dc61698b956499d0b98e8b1c5444
shovenyuan <shovenyuan@tencent.com>
2023-02-22 16:47:16 +0800

    添加base内容

--- practice/base.py
+++ practice/base.py
@@ -8,3 +8,17 @@ L2 = [x for x in L1 if isinstance(x, str)]
 print(L2)
 
 # 列表生成器
+
+
+# 过滤器求素数
+def _odd_filter():
+    n = 1
+    while True:
+        n = n + 2
+        yield n
+
+
+n = 1
+while n <= 10:
+    print(next(_odd_filter()))
+    n = n + 1
